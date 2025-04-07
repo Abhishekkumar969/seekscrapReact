7b74bf6201eb12df001ccf3c335e0c422e9a6db0
Abhishek kumar <123351889+Abhishekkumar969@users.noreply.github.com>
2025-04-08 00:50:37 +0530

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/images/logo.png";
-import styles from "../styles/Home.module.css"; // CSS Module
+import styles from "../styles/Home.module.css";
 
 function Home() {
     const navigate = useNavigate();
 
     useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("user"));
+
         const timer = setTimeout(() => {
-            navigate("/login");
+            if (user && user.role) {
+                navigate(`/${user.role}`);
+            } else {
+                navigate("/login");
+            }
         }, 3000);
 
         return () => clearTimeout(timer);
